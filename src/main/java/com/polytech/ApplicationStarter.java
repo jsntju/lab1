62b7b7e6f07b2245dc926ffcaa9f23d3e99a39c7
ju <jsnt.denis@gmail.com>
2017-03-17 08:54:30 +0100

    Code moins difficile (sans JSP) avec Thymleaf

--- src/main/java/com/polytech/ApplicationStarter.java
+++ src/main/java/com/polytech/ApplicationStarter.java
@@ -11,7 +11,7 @@ public class ApplicationStarter {
 
     public static void main(String[] args)
     {
-        System.setProperty("spring.profiles.active","PROD");
+        System.setProperty("spring.profiles.active","DEV");
         SpringApplication.run(ApplicationStarter.class,args);
     }
 }
